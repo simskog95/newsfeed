54a23f6648ab523355dd0865a416bedfdd76b7b6
Simon Skoglund <Simon Skoglund>
2016-12-01 03:01:32 +0100

    add expendable information on articleBoxContent when hover

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+$(window).resize(function() {
+	resizeArticleContent();
+});
+
+function resizeArticleContent() {
+	//change with of articleBoxContent to fitt within articleBox, not possible in css due to use of "position: absolute;" to eliminate bugg.
+	$(".articleBoxContent").width($(".articleBox").width()-10);
+}
+
 function addArticleToDom(h, c, h, d, i) {
 	
 }
